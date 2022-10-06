6f3158662d5a05bde2efe7f6f8767bb3e79f4672
Vagner Alcântara <vag_alc@hotmail.com>
2022-10-06 18:10:51 -0300

    Mais aulas

--- Functions.js
+++ Functions.js
@@ -2,9 +2,12 @@
 // function anonymous
 
 // parâmetros (paremeters)
+/*
 const sum = function (number1, number2) {
-    console.log(number1 + number2)
+    total = number1 + number2
+    return total
 }
+
 
 let number1 = 34
 let number2 = 25
@@ -12,3 +15,15 @@ let number2 = 25
 console.log(`O Number 1 é: ${number1}`)
 console.log(`O Number 2 é: ${number2}`)
 console.log(`A soma é: ${sum (number1, number2)}`)
+*/
+
+// Function Scope
+
+let subject = 'create video'
+
+function createThink(subject) {
+    return subject
+}
+
+console.log(subject)
+console.log(createThink(subject))
